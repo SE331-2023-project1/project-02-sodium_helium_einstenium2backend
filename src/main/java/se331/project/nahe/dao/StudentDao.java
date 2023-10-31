307c0e7ef645568231ec51fa82ca7cc50a5bbee1
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-10-31 11:04:26 +0700

    update dao student

--- src/main/java/se331/project/nahe/dao/StudentDao.java
+++ src/main/java/se331/project/nahe/dao/StudentDao.java
@@ -1,15 +1,20 @@
 package se331.project.nahe.dao;
 
-import se331.project.nahe.entity.Student;
+import com.se331.nahe.entity.Student;
+import com.se331.nahe.entity.StudentDTO;
+import com.se331.nahe.entity.Teacher;
+import com.se331.nahe.security.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-public interface StudentDao {
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+public interface StudentDao {
     Integer getStudentSize();
     Student save(Student student);
     Page<Student> getStudents(Integer pageSize, Integer page);
-    Page<Student> getStudents(String name, Pageable page);
+    Page<Student> getStudents(String filter, Pageable page);
     Student getStudent(Long id);
-
-    
+    User updateStudent(Long id, User updatedUser);
 }
--- src/main/java/se331/project/nahe/dao/StudentDaoImpl.java
+++ src/main/java/se331/project/nahe/dao/StudentDaoImpl.java
@@ -1,20 +1,35 @@
 package se331.project.nahe.dao;
 
-import se331.project.nahe.entity.Student;
-import se331.project.nahe.repository.StudentRepository;
+import com.se331.nahe.entity.Student;
+import com.se331.nahe.entity.StudentDTO;
+import com.se331.nahe.entity.Teacher;
+import com.se331.nahe.repository.StudentRepository;
+import com.se331.nahe.security.user.User;
+import com.se331.nahe.security.user.UserRepository;
+import com.se331.nahe.util.CloudStorageHelper;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.ServletException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 @Profile("db")
-public class StudentDaoImpl implements StudentDao {
-
+public class StudentDaoDbImpl implements StudentDao {
     final StudentRepository studentRepository;
+    final UserRepository userRepository;
+    @Autowired
+    private CloudStorageHelper cloudStorageHelper;
     @Override
     public Integer getStudentSize() {
         return Math.toIntExact(studentRepository.count());
@@ -31,8 +46,8 @@ public class StudentDaoImpl implements StudentDao {
     }
 
     @Override
-    public Page<Student> getStudents(String name, Pageable page) {
-        return null;
+    public Page<Student> getStudents(String filter, Pageable page) {
+        return studentRepository.findByUser_FirstnameIgnoreCaseContainingOrUser_LastnameIgnoreCaseContainingOrUser_UsernameIgnoreCaseContaining(filter,filter,filter, page);
     }
 
     @Override
@@ -40,4 +55,31 @@ public class StudentDaoImpl implements StudentDao {
         return studentRepository.findById(id).orElse(null);
     }
 
+    @Override
+    public User updateStudent(Long id, User updatedUser) {
+        return studentRepository.findById(id)
+                .map(student -> {
+                    User user = student.getUser();
+                    if (updatedUser.getFirstname() != null) {
+                        user.setFirstname(updatedUser.getFirstname());
+                    }
+                    if (updatedUser.getLastname() != null) {
+                        user.setLastname(updatedUser.getLastname());
+                    }
+                    if (updatedUser.getUsername() != null) {
+                        user.setUsername(updatedUser.getUsername());
+                    }
+                    if (updatedUser.getEmail() != null) {
+                        user.setEmail(updatedUser.getEmail());
+                    }
+                    if (updatedUser.getPassword() != null) {
+                        user.setPassword(updatedUser.getPassword());
+                    }
+                    if (updatedUser.getImage() != null) {
+                        user.setImage(updatedUser.getImage());
+                    }
+                    return userRepository.save(user);
+                })
+                .orElse(null);
+    }
 }
