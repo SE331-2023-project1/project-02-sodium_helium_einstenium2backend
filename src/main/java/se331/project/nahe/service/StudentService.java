c0c632562903b425d04ec7250217a30b7299a26f
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-10-31 12:18:46 +0700

    update student service

--- src/main/java/se331/project/nahe/service/StudentService.java
+++ src/main/java/se331/project/nahe/service/StudentService.java
@@ -1,23 +1,30 @@
-package se331.project.nahe.service;
+package com.se331.nahe.service;
 
-
+import com.se331.nahe.entity.StudentDTO;
+import com.se331.nahe.entity.Teacher;
+import com.se331.nahe.security.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import se331.project.nahe.entity.Student;
+import com.se331.nahe.entity.Student;
+import org.springframework.web.multipart.MultipartFile;
 
-
+import java.util.List;
 
 public interface StudentService {
-
     Integer getStudentsSize();
 
     Page<Student> getStudents(Integer pageSize, Integer page);
 
-    Page<Student> getStudents(String title, Pageable pageable);
+    Page<Student> getStudents(String filter, Pageable pageable);
 
     Student getStudent(Long id);
 
     Student save(Student student);
 
+    User updateStudent(Long id, User user);
+
+    User findUserByStudentId(Long studentUserId);
+
+    Page<Student> getStudentTeacherIsNull(Integer perPage, Integer page);
 }
--- src/main/java/se331/project/nahe/service/StudentServiceImpl.java
+++ src/main/java/se331/project/nahe/service/StudentServiceImpl.java
@@ -1,17 +1,24 @@
-package se331.project.nahe.service;
+package com.se331.nahe.service;
 
-
-import se331.project.nahe.dao.StudentDao;
-import se331.project.nahe.entity.Student;
+import com.se331.nahe.dao.StudentDao;
+import com.se331.nahe.entity.Student;
+import com.se331.nahe.entity.StudentDTO;
+import com.se331.nahe.repository.StudentRepository;
+import com.se331.nahe.security.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
     final StudentDao studentDao;
+    private final StudentRepository studentRepository;
     @Override
     public Integer getStudentsSize() {
         return studentDao.getStudentSize();
@@ -23,8 +30,8 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public Page<Student> getStudents(String title, Pageable pageable) {
-        return null;
+    public Page<Student> getStudents(String filter, Pageable pageable) {
+        return studentDao.getStudents(filter,pageable);
     }
 
     @Override
@@ -35,5 +42,24 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public Student save(Student student) {
         return studentDao.save(student);
+    }
+
+    @Override
+    public User updateStudent(Long id, User user) {
+        return studentDao.updateStudent(id, user);
+    }
+
+    @Override
+    public User findUserByStudentId(Long studentId) {
+        return studentRepository.findById(studentId)
+                .map(Student::getUser)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found"));
+    }
+
+
+
+    @Override
+    public Page<Student> getStudentTeacherIsNull(Integer perPage, Integer page) {
+        return studentRepository.findByTeacherIsNull(PageRequest.of(page - 1, perPage));
     }
 }
