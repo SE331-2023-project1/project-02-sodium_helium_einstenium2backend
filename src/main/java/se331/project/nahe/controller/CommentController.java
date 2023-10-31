4845156dc85467509796c8f774f20230c06f0c25
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-10-31 11:00:22 +0700

    update controller

---
+++ src/main/java/se331/project/nahe/controller/CommentController.java
@@ -0,0 +1,74 @@
+package com.se331.nahe.controller;
+
+import com.se331.nahe.entity.Comment;
+import com.se331.nahe.entity.CommentDTO;
+import com.se331.nahe.entity.CommentRequest;
+import com.se331.nahe.repository.CommentRepository;
+import com.se331.nahe.service.CommentService;
+import com.se331.nahe.util.LabMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<CommentDTO> createComment(@RequestParam Long studentId, @RequestParam Long teacherId, @RequestBody CommentRequest request) {
+        String content = request.getContent();
+        CommentDTO commentDTO = LabMapper.INSTANCE.commentToCommentDTO(commentService.createComment(studentId, teacherId, content));
+        if (commentDTO != null) {
+            return ResponseEntity.ok(commentDTO);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/reply")
+    public ResponseEntity<CommentDTO> replyComment(@RequestParam Long commentId,@RequestParam Long studentId,@RequestParam Long teacherId,  @RequestBody CommentRequest request) {
+        String content = request.getContent();
+        CommentDTO replyDTO = LabMapper.INSTANCE.commentToCommentDTO(commentService.replyComment(studentId, teacherId, commentId, content));
+        if (replyDTO != null) {
+            return ResponseEntity.ok(replyDTO);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+@GetMapping("/student/{studentId}")
+public ResponseEntity<List<CommentDTO>> getAllCommentsByStudentId(@PathVariable Long studentId) {
+    List<Comment> allComments = commentService.getAllCommentsByStudentId(studentId);
+
+    List<CommentDTO> mainCommentsDTO = allComments.stream()
+            .filter(comment -> comment.getParentComment() == null)
+            .map(comment -> {
+                CommentDTO mainCommentDTO = LabMapper.INSTANCE.commentToCommentDTO(comment);
+
+                Comment replyComment = allComments.stream()
+                        .filter(c -> c.getParentComment() != null && c.getParentComment().getId().equals(comment.getId()))
+                        .findFirst()
+                        .orElse(null);
+
+                if(replyComment != null) {
+                    CommentDTO replyDTO = LabMapper.INSTANCE.commentToCommentDTO(replyComment);
+                    mainCommentDTO.setReply(replyDTO);
+                }
+
+                return mainCommentDTO;
+            })
+            .collect(Collectors.toList());
+
+    return ResponseEntity.ok(mainCommentsDTO);
+}
+}
---
+++ src/main/java/se331/project/nahe/controller/UserController.java
@@ -0,0 +1,188 @@
+package com.se331.nahe.controller;
+
+import com.se331.nahe.dao.StudentDao;
+import com.se331.nahe.entity.DetailedTeacherDTO;
+import com.se331.nahe.entity.Student;
+import com.se331.nahe.entity.StudentDTO;
+import com.se331.nahe.entity.Teacher;
+import com.se331.nahe.exception.StudentNotFoundException;
+import com.se331.nahe.exception.TeacherNotFoundException;
+import com.se331.nahe.exception.UserNotFoundException;
+import com.se331.nahe.repository.StudentRepository;
+import com.se331.nahe.repository.TeacherRepository;
+import com.se331.nahe.security.user.User;
+import com.se331.nahe.security.user.UserRepository;
+import com.se331.nahe.security.user.UserService;
+import com.se331.nahe.service.StudentService;
+import com.se331.nahe.service.TeacherService;
+import com.se331.nahe.util.LabMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final StudentRepository studentRepository;
+    private final UserRepository userRepository;
+    private final TeacherRepository teacherRepository;
+
+    private final TeacherService teacherService;
+    private final StudentService studentService;
+    private final UserService userService;
+
+
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(users));
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<?> getAllStudents(@RequestParam(value = "_limit", required = false) Integer perPage,
+                                            @RequestParam(value = "_page", required = false) Integer page,
+                                            @RequestParam(value = "_filter", required = false) String filter,
+                                            @RequestParam(value = "_haveNoTeacher", required = false) Boolean haveNoTeacher) {
+        perPage = perPage == null ? 20 : perPage;
+        page = page == null ? 1 : page;
+        Page<Student> pageOutput;
+
+        Long idFilter = null;
+        if (filter != null && filter.matches("\\d+")) {
+            idFilter = Long.parseLong(filter);
+        }
+
+
+        if (idFilter != null) {
+            pageOutput = studentRepository.findById(idFilter,PageRequest.of(page - 1, perPage) );
+        } else if (filter != null) {
+            pageOutput = studentService.getStudents(filter, PageRequest.of(page - 1, perPage));
+        } else {
+            pageOutput = studentService.getStudents(perPage, page);
+        }
+
+        if (haveNoTeacher != null) {
+            pageOutput = studentService.getStudentTeacherIsNull(perPage, page);
+        }
+
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+        return new ResponseEntity<>(pageOutput.getContent().stream()
+                .map(LabMapper.INSTANCE::getStudentDTO)
+                .collect(Collectors.toList()), responseHeader, HttpStatus.OK);
+
+    }
+
+    @PutMapping("/relation")
+    public ResponseEntity<?> changeRelation(@RequestParam(value = "teacherId") Integer teacherId,
+                                            @RequestParam(value = "studentId") Integer studentId) {
+        ;
+    return ResponseEntity.ok(LabMapper.INSTANCE.getTeacherDTO(teacherService.addStudent(studentId,teacherId)));
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+        Student studentopt = studentService.getStudent(id);
+        if (studentopt == null) {
+            throw new StudentNotFoundException(id);
+        }
+        return ResponseEntity.ok(LabMapper.INSTANCE.getStudentDTO(studentopt));
+    }
+
+
+    @PutMapping(value = "/students/{id}")
+    public ResponseEntity<?> updateStudent(@PathVariable("id") Long id,
+                                           @RequestBody User user) {
+        User updatedUser = studentService.updateStudent(id, user);
+        if (updatedUser == null) {
+            throw new UserNotFoundException(id);
+        } else {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(updatedUser));
+        }
+    }
+
+
+    @GetMapping("/teachers")
+    public ResponseEntity<?> getAllTeachers(@RequestParam(value = "_limit", required = false) Integer perPage,
+                                            @RequestParam(value = "_page", required = false) Integer page,
+                                            @RequestParam(value = "_filter", required = false) String filter) {
+        perPage = perPage == null ? 20 : perPage;
+        page = page == null ? 1 : page;
+        Page<Teacher> pageOutput;
+
+        Long idFilter = null;
+        if (filter != null && filter.matches("\\d+")) {
+            idFilter = Long.parseLong(filter);
+        }
+
+        if (idFilter != null) {
+            pageOutput = teacherRepository.findById(idFilter,PageRequest.of(page - 1, perPage) );
+        }
+        else if (filter != null) {
+            pageOutput = teacherService.getTeachers(filter, PageRequest.of(page - 1, perPage));
+
+        } else {
+            pageOutput = teacherService.getTeachers(perPage, page);
+
+        }
+        HttpHeaders responseHeader = new HttpHeaders();
+        responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+        return new ResponseEntity<>(pageOutput.getContent().stream()
+                .map(LabMapper.INSTANCE::getDetailedTeacherDTO)
+                .collect(Collectors.toList()), responseHeader, HttpStatus.OK);
+
+
+    }
+
+    @GetMapping("/teachers/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<?> getTeacherById(@PathVariable("id") Long id) {
+        Teacher teacherOpt = teacherService.getTeacher(id);
+
+        if (teacherOpt == null) {
+            throw new TeacherNotFoundException(id);
+        } else {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getDetailedTeacherDTO(teacherOpt));
+        }
+
+    }
+
+    @PutMapping(value = "/teachers/{id}")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity<?> updateTeacher(@PathVariable("id") Long id,
+                                           @RequestBody User user
+                                          ) {
+        User updatedUser = teacherService.updateTeacher(id, user);
+        if (updatedUser == null) {
+            throw new UserNotFoundException(id);
+        } else {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(updatedUser));
+        }
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUser(@PathVariable("id") Long id) {
+        User user = userService.getUser(id);
+        if (user != null) {
+            return ResponseEntity.ok(LabMapper.INSTANCE.getUserDTO(user));
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+    }
+
+
+}
