e740ff18af27f219ab43256edcba81a62578e027
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-10-31 11:50:02 +0700

    add security config

---
+++ src/main/java/se331/project/nahe/security/config/SecurityConfiguration.java
@@ -0,0 +1,61 @@
+package com.se331.nahe.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.http.HttpMethod;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.headers((headers) -> {
+            headers.frameOptions((frameOptions) -> frameOptions.disable());
+        });
+        http
+                .csrf((crsf) -> crsf.disable())
+                .authorizeHttpRequests((authorize) -> {
+
+                    authorize.requestMatchers("/**").permitAll()
+                            .requestMatchers("/uploadImage").permitAll()
+                            .requestMatchers("/api/v1/auth/**").permitAll()
+                            .requestMatchers("/api/v1/**").permitAll()
+                            .requestMatchers("/api/v1/announcements").permitAll()
+                            .requestMatchers("/users/**").permitAll()
+                            .requestMatchers(HttpMethod.GET,"/teachers").permitAll()
+                            .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .sessionManagement((session) -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout((logout) -> {
+                    logout.logoutUrl("/api/v1/auth/logout");
+                    logout.addLogoutHandler(logoutHandler);
+                    logout.logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
+                })
+        ;
+        http.cors(withDefaults());
+        return http.build();
+
+    }
+}
