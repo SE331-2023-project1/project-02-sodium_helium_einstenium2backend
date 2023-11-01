f6dac05a06e261cef6dd67d9c1f00bec990841a2
tommyyeebai <115710017+tommyyeebai@users.noreply.github.com>
2023-11-01 11:33:34 +0700

    update test

---
+++ src/test/java/com/se331/nahe/SodiumHeliumEinsteniumApplicationTests.java
@@ -0,0 +1,12 @@
+package com.se331.nahe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodiumHeliumEinsteniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
